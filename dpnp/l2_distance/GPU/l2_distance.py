8a9effcb18af9183b3d65b2facc034c3b8c81f29
Adarsh Yoga <adarsh.yoga@intel.com>
2021-02-01 18:19:44 +0000

    adding l2 distance codes

---
+++ dpnp/l2_distance/GPU/l2_distance.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2017-2018 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+import dpctl
+import base_l2_distance
+import dpnp as np
+
+def l2_distance(a,b):
+    sub = a-b
+    sq = np.square(sub)
+    sum = np.sum(sq)
+    d = np.sqrt(sum)
+    return d
+
+def l2_distance_dpctl(a,b):
+    with dpctl.device_context("opencl:gpu"):
+        l2_distance(a,b)
+        
+base_l2_distance.run("l2 distance", l2_distance_dpctl)

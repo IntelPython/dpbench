64fbc5601035aa94447c843969f3c3a371b0c4d1
Adarsh Yoga <adarsh.yoga@intel.com>
2021-02-18 17:12:58 +0000

    adding dpcpp versions of bscholes

---
+++ native_dpcpp/blackscholes/CPU/euro_opt.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2014-2015, 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef __EURO_OPT_BENCH_H
+#define __EURO_OPT_BENCH_H
+
+#include <CL/sycl.hpp>
+
+#ifdef __DO_FLOAT__
+    typedef float tfloat; 
+#else
+    typedef double  tfloat;
+#endif
+
+#define ALIGN_FACTOR 64
+
+#define SEED 7777777
+
+#define S0L     10.0f
+#define S0H     50.0f
+#define XL      10.0f
+#define XH      50.0f
+
+#define TL      1.0f
+#define TH      2.0f
+
+#define RISK_FREE  0.1f
+#define VOLATILITY 0.2f
+
+//using namespace cl::sycl;
+
+void InitData( cl::sycl::queue* q, size_t nopt, tfloat* *s0, tfloat* *x, tfloat* *t,
+                   tfloat* *vcall_compiler, tfloat* *vput_compiler,
+                   tfloat* *vcall_mkl, tfloat* *vput_mkl
+             );
+
+void FreeData( cl::sycl::queue* q, tfloat *s0, tfloat *x, tfloat *t,
+                   tfloat *vcall_compiler, tfloat *vput_compiler,
+                   tfloat *vcall_mkl, tfloat *vput_mkl
+             );
+
+void BlackScholesNaive( size_t nopt, tfloat r, tfloat sig, const tfloat so[],
+    const tfloat x[], const tfloat t[], tfloat vcall[], tfloat vput[] );
+
+void BlackScholesFormula_Compiler( size_t nopt, cl::sycl::queue* q,
+    tfloat r, tfloat sig, tfloat * s0, tfloat * x,
+    tfloat * t, tfloat * vcall, tfloat * vput );
+
+void BlackScholesFormula_MKL( size_t nopt,
+    tfloat r, tfloat sig, tfloat *  s0, tfloat *  x,
+    tfloat *  t, tfloat *  vcall, tfloat *  vput );
+
+void BlackScholesFormula_CND_TBB( size_t nopt,
+    tfloat r, tfloat sig, tfloat *  s0, tfloat *  x,
+    tfloat *  t, tfloat *  vcall, tfloat *  vput );
+
+void BlackScholesFormula_CND( size_t nopt,
+    tfloat r, tfloat sig, tfloat *  s0, tfloat *  x,
+    tfloat *  t, tfloat *  vcall, tfloat *  vput );
+
+#endif // #ifndef __EURO_OPT_BENCH_H

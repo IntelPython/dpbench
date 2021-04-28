b0f9f7d5d78117adb7d55e1a6b0fa5bfdba27727
Adarsh Yoga <adarsh.yoga@intel.com>
2021-04-28 01:59:30 +0000

    updates to add comments to pairwise distance

--- numba/pairwise_distance/GPU/pairwise_distance.py
+++ numba/pairwise_distance/GPU/pairwise_distance.py
@@ -7,17 +7,24 @@ import base_pair_wise
 import numpy as np
 import numba
 
+# Naieve pairwise distance impl - take an array representing M points in N dimensions, and return the M x M matrix of Euclidean distances
 @numba.njit(parallel=True,fastmath=True)
 def pw_distance_kernel(X1,X2,D):
+    # Size of imputs
     M = X1.shape[0]
     N = X2.shape[0]
     O = X1.shape[1]
+
+    # Outermost parallel loop over the matrix X1
     for i in numba.prange(M):
+        # Loop over the matrix X2
         for j in range(N):
             d = 0.0
+            #Compute exclidean distance
             for k in range(O):
                 tmp = X1[i, k] - X2[j, k]
                 d += tmp * tmp
+            # Write computed distance to distance matrix
             D[i, j] = np.sqrt(d)
 
 def pw_distance(X1,X2,D):

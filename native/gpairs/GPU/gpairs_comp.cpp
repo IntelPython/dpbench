dc9094ce33729c26148f258a612d329b83918dd9
Adarsh Yoga <adarsh.yoga@intel.com>
2021-08-02 22:28:40 +0000

    adding gpairs comp only version

---
+++ native/gpairs/GPU/gpairs_comp.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2014-2015, 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <omp.h>
+#include "euro_opt.h"
+
+void call_gpairs( size_t npoints, tfloat* x1, tfloat* y1, tfloat* z1, tfloat* w1, tfloat* x2,tfloat* y2,tfloat* z2, tfloat* w2,tfloat* rbins,tfloat* results_test) {
+
+  int nbins = DEFAULT_NBINS;
+  
+#pragma omp target teams distribute					\
+  parallel for simd
+  for (unsigned int i = 0; i < npoints; i++) {
+
+    tfloat px = x1[i];
+    tfloat py = y1[i];
+    tfloat pz = z1[i];
+    tfloat pw = w1[i];
+    for (unsigned int j = 0; j < npoints; j++) {
+      tfloat qx = x2[j];
+      tfloat qy = y2[j];
+      tfloat qz = z2[j];
+      tfloat qw = w2[j];
+      tfloat dx = px - qx;
+      tfloat dy = py - qy;
+      tfloat dz = pz - qz;
+      tfloat wprod = pw * qw;
+      tfloat dsq = dx*dx + dy*dy + dz*dz;
+
+      int k = nbins - 1;
+      while(dsq <= rbins[k]) {
+#pragma omp atomic update
+	results_test[k-1] += wprod;
+	k = k-1;
+	if (k <=0) break;
+      }
+    }
+  }
+}
---
+++ native/gpairs/GPU/main_comp.cpp
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2014-2015, 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "euro_opt.h"
+#include "rdtsc.h"
+
+int main(int argc, char * argv[])
+{
+    size_t nopt = 1024;
+    int repeat = 1;
+    int STEPS = 3;
+
+    clock_t t1 = 0, t2 = 0;
+
+    /* Read nopt number of options parameter from command line */
+    if (argc < 2)
+    {
+        printf("Usage: expect STEPS input integer parameter, defaulting to %d\n", STEPS);
+    }
+    else
+    {
+        sscanf(argv[1], "%d", &STEPS);
+	if (argc == 3) {
+	  sscanf(argv[2], "%lu", &nopt);
+	}
+	if (argc == 4) {
+	  sscanf(argv[3], "%d", &repeat);
+	}	
+    }
+
+    FILE *fptr;
+    fptr = fopen("perf_output.csv", "w");
+    if(fptr == NULL) {
+      printf("Error!");   
+      exit(1);             
+    }
+
+    FILE *fptr1;
+    fptr1 = fopen("runtimes.csv", "w");
+    if(fptr1 == NULL) {
+      printf("Error!");   
+      exit(1);             
+    }
+    tfloat *x1, *y1, *z1, *w1, *x2, *y2, *z2, *w2, *rbins, *results_test;
+    int i, j;
+    for(i = 0; i < STEPS; i++) {
+      /* Allocate arrays, generate input data */
+      InitData( nopt, &x1, &y1, &z1, &w1, &x2, &y2, &z2, &w2, &rbins, &results_test);
+
+#pragma omp target data map(to:x1[0:nopt],y1[0:nopt],z1[0:nopt],w1[0:nopt],x2[0:nopt],y2[0:nopt],z2[0:nopt],w2[0:nopt],rbins[0:DEFAULT_NBINS]) map(tofrom:results_test[0:(DEFAULT_NBINS-1)])
+      {
+	/* Warm up cycle */
+	for(j = 0; j < 1; j++) {
+	  call_gpairs( nopt, x1, y1, z1, w1, x2, y2, z2, w2, rbins, results_test);
+	}
+
+	if (repeat < 1) repeat = 1;
+
+	t1 = timer_rdtsc();
+	for(j = 0; j < repeat; j++) {
+	  call_gpairs( nopt, x1, y1, z1, w1, x2, y2, z2, w2, rbins, results_test );
+	}
+	t2 = timer_rdtsc();
+      }
+      
+      printf("%lu,%.6lf\n",nopt,((double) (t2 - t1) / getHz()));
+      fflush(stdout);
+      fprintf(fptr, "%lu,%.6lf\n",nopt,(2.0 * nopt * repeat )/((double) (t2 - t1) / getHz()));
+      fprintf(fptr1, "%lu,%.6lf\n",nopt,((double) (t2 - t1) / getHz()));
+
+#if 0 //print result
+      for (size_t i = 0; i < (DEFAULT_NBINS-1); i++) {
+	printf("%lf\n",results_test[i]);
+      }
+#endif
+      
+      /* Deallocate arrays */
+      FreeData( x1, y1, z1, w1, x2, y2, z2, w2, rbins, results_test );
+
+      nopt = nopt * 2;
+      if (repeat > 2) repeat -= 2;
+    }
+    
+    fclose(fptr);
+    fclose(fptr1);
+
+    return 0;
+}

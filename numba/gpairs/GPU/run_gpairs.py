741ca60d1ea4c458d9abc99118a33f50366debed
reazul.hoque <reazul.hoque@intel.com>
2021-09-08 13:54:26 -0500

    update cuda

--- numba/gpairs/GPU/run_gpairs.py
+++ numba/gpairs/GPU/run_gpairs.py
@@ -3,15 +3,6 @@ import numpy as np
 import gaussian_weighted_pair_counts as gwpc
 import numba_dppy
 import dpctl
-# from gpairs.pair_counter.tests.generate_test_data import (
-#     DEFAULT_RBINS_SQUARED)
-
-# DEFAULT_NBINS = 20
-# DEFAULT_RMIN, DEFAULT_RMAX = 0.1, 50
-# DEFAULT_RBINS = np.logspace(
-#     np.log10(DEFAULT_RMIN), np.log10(DEFAULT_RMAX), DEFAULT_NBINS).astype(
-#         np.float32)
-# DEFAULT_RBINS_SQUARED = (DEFAULT_RBINS**2).astype(np.float32)
 
 def run_gpairs(d_x1, d_y1, d_z1, d_w1, d_x2, d_y2, d_z2, d_w2, d_rbins_squared, result):
     blocks = 512
@@ -24,5 +15,5 @@ def run_gpairs(d_x1, d_y1, d_z1, d_w1, d_x2, d_y2, d_z2, d_w2, d_rbins_squared, 
         gwpc.count_weighted_pairs_3d_intel_ver2[d_x1.shape[0], numba_dppy.DEFAULT_LOCAL_SIZE](
             d_x1, d_y1, d_z1, d_w1, d_x2, d_y2, d_z2, d_w2,
             d_rbins_squared, result)
-        
+
 base_gpairs.run("Gpairs Dppy kernel",run_gpairs)
--- numba/gpairs/GPU/run_gpairs_cuda.py
+++ numba/gpairs/GPU/run_gpairs_cuda.py
@@ -3,21 +3,10 @@ import numpy as np
 from numba import cuda
 from gaussian_weighted_pair_counts import count_weighted_pairs_3d_cuda
 
-DEFAULT_NBINS = 20
-DEFAULT_RMIN, DEFAULT_RMAX = 0.1, 50
-DEFAULT_RBINS = np.logspace(
-    np.log10(DEFAULT_RMIN), np.log10(DEFAULT_RMAX), DEFAULT_NBINS).astype(
-        np.float32)
-DEFAULT_RBINS_SQUARED = (DEFAULT_RBINS**2).astype(np.float32)
-
-def run_gpairs(d_x1, d_y1, d_z1, d_w1, d_x2, d_y2, d_z2, d_w2, d_rbins_squared):
+def run_gpairs(d_x1, d_y1, d_z1, d_w1, d_x2, d_y2, d_z2, d_w2, d_rbins_squared, d_result):
     blocks = 512
     threads = 512
 
-    result = np.zeros_like(DEFAULT_RBINS_SQUARED)[:-1]
-    result = result.astype(np.float32)
-
-    d_result = cuda.device_array_like(result.astype(np.float64))
     count_weighted_pairs_3d_cuda[blocks, threads](
         d_x1, d_y1, d_z1, d_w1, d_x2, d_y2, d_z2, d_w2,
         d_rbins_squared, d_result)

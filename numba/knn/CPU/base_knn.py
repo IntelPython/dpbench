8f07446c0c87fb5f4f5f42f7c82fa4f18d89b77e
Butygin <ivan.butygin@intel.com>
2021-10-27 16:36:44 +0300

    fix knn

--- numba/knn/CPU/base_knn.py
+++ numba/knn/CPU/base_knn.py
@@ -30,13 +30,14 @@ import numpy as np
 import numpy.random as rnd
 
 from knn_python import knn_python
-from dpbench_datagen.knn import gen_data_x, gen_data_y
-
-DATA_DIM = 16
-SEED = 7777777
-CLASSES_NUM = 3
-TRAIN_DATA_SIZE = 2 ** 10
-n_neighbors = 5
+from dpbench_datagen.knn import (
+    gen_train_data,
+    gen_test_data,
+    CLASSES_NUM,
+    DATA_DIM,
+    TRAIN_DATA_SIZE,
+    N_NEIGHBORS,
+)
 
 try:
     import itimer as it
@@ -101,10 +102,8 @@ def run(name, alg, sizes=10, step=2, nopt=2 ** 10):
     rnd.seed(SEED)
 
     if args.test:
-        x_train, y_train = gen_data_x(
-            TRAIN_DATA_SIZE, seed=0, dim=DATA_DIM
-        ), gen_data_y(TRAIN_DATA_SIZE, CLASSES_NUM, seed=0)
-        x_test = gen_data_x(nopt, seed=777777, dim=DATA_DIM)
+        x_train, y_train = gen_train_data()
+        x_test = gen_test_data(nopt)
 
         print("TRAIN: ", x_train[:10])
         print(y_train[:10])
@@ -123,10 +122,8 @@ def run(name, alg, sizes=10, step=2, nopt=2 ** 10):
     with open("perf_output.csv", "w", 1) as fd, open("runtimes.csv", "w", 1) as fd2:
         for _ in xrange(args.steps):
 
-            x_train, y_train = gen_data_x(
-                TRAIN_DATA_SIZE, seed=0, dim=DATA_DIM
-            ), gen_data_y(TRAIN_DATA_SIZE, CLASSES_NUM, seed=0)
-            x_test = gen_data_x(nopt, seed=777777, dim=DATA_DIM)
+            x_train, y_train = gen_train_data()
+            x_test = gen_test_data(nopt)
 
             sys.stdout.flush()
 

60ccc3208de546b04a067aff1b5b6b1c906f61e7
Butygin <ivan.butygin@intel.com>
2021-10-27 15:53:05 +0300

    fix

--- utils/run_utils.py
+++ utils/run_utils.py
@@ -132,6 +132,6 @@ def run_command(
             print(output)
     except subprocess.CalledProcessError as e:
         if throw_exception:
-            raise ExperimentError(command_string, e.output)
+            raise ExperimentError(command_string, str(e))
     else:
         return output

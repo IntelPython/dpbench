677f0335ae40de303d03409803479f7c6ca12c88
Adarsh Yoga <adarshyoga@gmail.com>
2021-11-30 15:58:55 -0800

    adding knn cpu versions

--- numba/knn/CPU/knn.py
+++ numba/knn/CPU/knn.py
@@ -24,85 +24,172 @@
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # *****************************************************************************
 
-import numpy as np
-import numba
 import base_knn
-from dpbench_decorators import jit
+import dpctl
+import numba_dppy
+import math
+
+import dpctl.tensor as dpt
 
 
-@jit(nopython=True)
-def euclidean_dist(x1, x2):
+# @numba.jit(nopython=True)
+# def euclidean_dist(x1, x2):
+#     return np.linalg.norm(x1-x2)
+
+
+@numba_dppy.func
+def euclidean_dist(x1, x2, data_dim):
     distance = 0
 
-    for i in range(len(x1)):
+    for i in range(data_dim):
         diff = x1[i] - x2[i]
         distance += diff * diff
 
     result = distance ** 0.5
-    # result = np.sqrt(distance)
     return result
 
 
-@jit(nopython=True)
-def push_queue(queue_neighbors, new_distance, index=4):
-    while index > 0 and new_distance[0] < queue_neighbors[index - 1][0]:
+@numba_dppy.func
+def push_queue(queue_neighbors, new_distance, index=4):  # 4: k-1
+    while index > 0 and new_distance[0] < queue_neighbors[index - 1, 0]:
         queue_neighbors[index] = queue_neighbors[index - 1]
         index = index - 1
         queue_neighbors[index] = new_distance
 
 
-@jit(nopython=True)
+@numba_dppy.func
 def sort_queue(queue_neighbors):
     for i in range(len(queue_neighbors)):
         push_queue(queue_neighbors, queue_neighbors[i], i)
 
 
-@jit(nopython=True)
-def simple_vote(neighbors, classes_num=3):
-    votes_to_classes = np.zeros(classes_num)
+@numba_dppy.kernel(
+    access_types={
+        "read_only": [
+            "train",
+            "train_labels",
+            "test",
+            "votes_to_classes_lst",
+            "queue_neighbors_lst",
+        ],
+        "write_only": ["predictions"],
+    }
+)
+def run_knn_kernel(
+    train,
+    train_labels,
+    test,
+    k,
+    classes_num,
+    train_size,
+    predictions,
+    queue_neighbors_lst,
+    votes_to_classes_lst,
+    data_dim,
+):
+    i = numba_dppy.get_global_id(0)
+    queue_neighbors = queue_neighbors_lst[i]
 
-    for neighbor in neighbors:
-        votes_to_classes[neighbor[1]] += 1
+    for j in range(k):
+        x1 = train[j]
+        x2 = test[i]
+
+        distance = 0.0
+        for jj in range(data_dim):
+            diff = x1[jj] - x2[jj]
+            distance += diff * diff
+        dist = math.sqrt(distance)
+
+        queue_neighbors[j, 0] = dist
+        queue_neighbors[j, 1] = train_labels[j]
+
+    # sort_queue(queue_neighbors)
+    # for j in range(len(queue_neighbors)):
+    for j in range(k):
+        # push_queue(queue_neighbors, queue_neighbors[i], i)
+        new_distance = queue_neighbors[j, 0]
+        new_neighbor_label = queue_neighbors[j, 1]
+        index = j
+
+        while index > 0 and new_distance < queue_neighbors[index - 1, 0]:
+            queue_neighbors[index, 0] = queue_neighbors[index - 1, 0]
+            queue_neighbors[index, 1] = queue_neighbors[index - 1, 1]
+
+            index = index - 1
+
+            queue_neighbors[index, 0] = new_distance
+            queue_neighbors[index, 1] = new_neighbor_label
+
+    for j in range(k, train_size):
+        # dist = euclidean_dist(train[j], test[i])
+        x1 = train[j]
+        x2 = test[i]
+
+        distance = 0.0
+        for jj in range(base_knn.DATA_DIM):
+            diff = x1[jj] - x2[jj]
+            distance += diff * diff
+        dist = math.sqrt(distance)
+
+        if dist < queue_neighbors[k - 1][0]:
+            queue_neighbors[k - 1][0] = dist
+            queue_neighbors[k - 1][1] = train_labels[j]
+            # push_queue(queue_neighbors, queue_neighbors[k - 1])
+            new_distance = queue_neighbors[k - 1, 0]
+            new_neighbor_label = queue_neighbors[k - 1, 1]
+            index = k - 1
+
+            while index > 0 and new_distance < queue_neighbors[index - 1, 0]:
+                queue_neighbors[index, 0] = queue_neighbors[index - 1, 0]
+                queue_neighbors[index, 1] = queue_neighbors[index - 1, 1]
+
+                index = index - 1
+
+                queue_neighbors[index, 0] = new_distance
+                queue_neighbors[index, 1] = new_neighbor_label
+
+    votes_to_classes = votes_to_classes_lst[i]
+
+    for j in range(k):
+        votes_to_classes[int(queue_neighbors[j, 1])] += 1
 
     max_ind = 0
     max_value = 0
 
-    for i in range(classes_num):
-        if votes_to_classes[i] > max_value:
-            max_value = votes_to_classes[i]
-            max_ind = i
+    for j in range(classes_num):
+        if votes_to_classes[j] > max_value:
+            max_value = votes_to_classes[j]
+            max_ind = j
 
-    return max_ind
+    predictions[i] = max_ind
 
 
-@jit(nopython=True, parallel=True)
-def run_knn(train, train_labels, test, k=5, classes_num=3):
-    test_size = len(test)
-    train_size = len(train)
-
-    predictions = np.empty(test_size)
-
-    for i in numba.prange(test_size):
-        queue_neighbors = []
-
-        for j in range(k):
-            dist = euclidean_dist(train[j], test[i])
-            # queue_neighbors[j] = (dist, train_labels[j])
-            queue_neighbors.append((dist, train_labels[j]))
-
-        sort_queue(queue_neighbors)
-
-        for j in range(k, train_size):
-            dist = euclidean_dist(train[j], test[i])
-            new_neighbor = (dist, train_labels[j])
-
-            if dist < queue_neighbors[k - 1][0]:
-                queue_neighbors[k - 1] = new_neighbor
-                push_queue(queue_neighbors, new_neighbor)
-
-        predictions[i] = simple_vote(queue_neighbors, classes_num)
-
-    return predictions
+def run_knn(
+    train,
+    train_labels,
+    test,
+    k,
+    classes_num,
+    test_size,
+    train_size,
+    predictions,
+    queue_neighbors_lst,
+    votes_to_classes_lst,
+    data_dim,
+):
+    with dpctl.device_context(base_knn.get_device_selector()) as gpu_queue:
+        run_knn_kernel[test_size, numba_dppy.DEFAULT_LOCAL_SIZE](
+            train,
+            train_labels,
+            test,
+            k,
+            classes_num,
+            train_size,
+            predictions,
+            queue_neighbors_lst,
+            votes_to_classes_lst,
+            data_dim,
+        )
 
 
 base_knn.run("K-Nearest-Neighbors Numba", run_knn)

e4f57e020cea56365705a14b2b9db124076c70e5
Adarsh Yoga <adarshyoga@gmail.com>
2021-11-30 16:01:35 -0800

    pairwise distance CPU version

--- numba/pairwise_distance/CPU/pw_numba.py
+++ numba/pairwise_distance/CPU/pw_numba.py
@@ -2,22 +2,34 @@
 #
 # SPDX-License-Identifier: MIT
 
-
+import dpctl
 import base_pair_wise
 import numpy as np
-from dpbench_decorators import jit
+import numba
 
 
-@jit(nopython=True, parallel=True, fastmath=True)
-def pw_distance(X1, X2, D):
+# Pairwise Numpy implementation using the equation (a-b)^2 = a^2 + b^2 - 2*a*b
+@numba.njit(parallel=True, fastmath=True)
+def pw_distance_kernel(X1, X2, D):
     # return np.sqrt((np.square(X1 - X2.reshape((X2.shape[0],1,X2.shape[1])))).sum(axis=2))
-    x1 = np.sum(np.square(X1), axis=1)  # X1=4*3 -> 4*1
-    x2 = np.sum(np.square(X2), axis=1)  # X2=4*3 -> 4*1
+
+    # Computing the first two terms (X1^2 and X2^2) of the Euclidean distance equation
+    x1 = np.sum(np.square(X1), axis=1)
+    x2 = np.sum(np.square(X2), axis=1)
+
+    # Comnpute third term in equation
     D = -2 * np.dot(X1, X2.T)
     x3 = x1.reshape(x1.size, 1)
-    D += x3  # x1[:,None] Not supported by Numba
-    D += x2
+    D = D + x3  # x1[:,None] Not supported by Numba
+    D = D + x2
+
+    # Compute square root for euclidean distance
     D = np.sqrt(D)
 
 
-base_pair_wise.run("Numba FastMath", pw_distance)
+def pw_distance(X1, X2, D):
+    with dpctl.device_context(base_pair_wise.get_device_selector()):
+        pw_distance_kernel(X1, X2, D)
+
+
+base_pair_wise.run("Numba Numpy", pw_distance)

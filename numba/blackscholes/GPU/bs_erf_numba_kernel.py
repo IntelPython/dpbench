cb31a130cb0f6751acfad7a798cb9b0be60bef3d
geexie <marina.kolpakova@intel.com>
2022-02-03 06:34:52 +0300

    runs blacksholes with backend selector

--- numba/blackscholes/GPU/bs_erf_numba_kernel.py
+++ numba/blackscholes/GPU/bs_erf_numba_kernel.py
@@ -2,47 +2,78 @@
 #
 # SPDX-License-Identifier: MIT
 
+import os
 import dpctl
 import base_bs_erf
 from device_selector import get_device_selector
-
-# import numba_dppy
-from numba_dpcomp.mlir.kernel_impl import kernel, get_global_id, DEFAULT_LOCAL_SIZE
 from math import log, sqrt, exp, erf
 
-# blackscholes implemented using dppy.kernel
-# @numba_dppy.kernel(
-#     access_types={"read_only": ["price", "strike", "t"], "write_only": ["call", "put"]}
-# )
-@kernel
-def black_scholes(nopt, price, strike, t, rate, vol, call, put):
-    mr = -rate
-    sig_sig_two = vol * vol * 2
+backend = os.getenv("NUMBA_BACKEND", "legacy")
+if backend == "legacy":
+    import numba_dppy
+    from numba_dppy import kernel, get_global_id, DEFAULT_LOCAL_SIZE
+    @numba_dppy.kernel(
+        access_types={"read_only": ["price", "strike", "t"], "write_only": ["call", "put"]}
+    )
+    def black_scholes(nopt, price, strike, t, rate, vol, call, put):
+        mr = -rate
+        sig_sig_two = vol * vol * 2
 
-    i = get_global_id(0)
+        i = numba_dppy.get_global_id(0)
 
-    P = price[i]
-    S = strike[i]
-    T = t[i]
+        P = price[i]
+        S = strike[i]
+        T = t[i]
 
-    a = log(P / S)
-    b = T * mr
+        a = log(P / S)
+        b = T * mr
 
-    z = T * sig_sig_two
-    c = 0.25 * z
-    y = 1.0 / sqrt(z)
+        z = T * sig_sig_two
+        c = 0.25 * z
+        y = 1.0 / sqrt(z)
 
-    w1 = (a - b + c) * y
-    w2 = (a - b - c) * y
+        w1 = (a - b + c) * y
+        w2 = (a - b - c) * y
 
-    d1 = 0.5 + 0.5 * erf(w1)
-    d2 = 0.5 + 0.5 * erf(w2)
+        d1 = 0.5 + 0.5 * erf(w1)
+        d2 = 0.5 + 0.5 * erf(w2)
 
-    Se = exp(b) * S
+        Se = exp(b) * S
 
-    r = P * d1 - Se * d2
-    call[i] = r
-    put[i] = r - P + Se
+        r = P * d1 - Se * d2
+        call[i] = r
+        put[i] = r - P + Se
+else:
+    from numba_dpcomp.mlir.kernel_impl import kernel, get_global_id, DEFAULT_LOCAL_SIZE
+    @kernel
+    def black_scholes(nopt, price, strike, t, rate, vol, call, put):
+        mr = -rate
+        sig_sig_two = vol * vol * 2
+
+        i = get_global_id(0)
+
+        P = price[i]
+        S = strike[i]
+        T = t[i]
+
+        a = log(P / S)
+        b = T * mr
+
+        z = T * sig_sig_two
+        c = 0.25 * z
+        y = 1.0 / sqrt(z)
+
+        w1 = (a - b + c) * y
+        w2 = (a - b - c) * y
+
+        d1 = 0.5 + 0.5 * erf(w1)
+        d2 = 0.5 + 0.5 * erf(w2)
+
+        Se = exp(b) * S
+
+        r = P * d1 - Se * d2
+        call[i] = r
+        put[i] = r - P + Se
 
 
 def black_scholes_driver(nopt, price, strike, t, rate, vol, call, put):

3a552b39bef6501dca9770397d918d92c9fb9ec9
Diptorup Deb <diptorup.deb@intel.com>
2022-06-09 17:52:27 -0500

    Port numba_o and numpy implementations for gpairs to new infrastructure.

---
+++ dpbench/benchmarks/gpairs/gpairs_numba_o.py
@@ -0,0 +1,36 @@
+# Copyright 2022 Intel Corp.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import numba as nb
+
+
+@nb.njit(parallel=False, fastmath=True)
+def gpairs(x1, y1, z1, w1, x2, y2, z2, w2, rbins, results):
+
+    n1 = x1.shape[0]
+    n2 = x2.shape[0]
+    nbins = rbins.shape[0]
+
+    for i in range(n1):
+        px = x1[i]
+        py = y1[i]
+        pz = z1[i]
+        pw = w1[i]
+        for j in range(n2):
+            qx = x2[j]
+            qy = y2[j]
+            qz = z2[j]
+            qw = w2[j]
+            dx = px - qx
+            dy = py - qy
+            dz = pz - qz
+            wprod = pw * qw
+            dsq = dx * dx + dy * dy + dz * dz
+
+            k = nbins - 1
+            while dsq <= rbins[k]:
+                results[k - 1] += wprod
+                k = k - 1
+                if k <= 0:
+                    break

1c591d46a5661c0ac13dfc7f8ed80a11a0f34f4e
Adarsh Yoga <adarsh.yoga@intel.com>
2022-07-13 18:09:24 +0000

    pre-commit changes

--- dpbench/benchmarks/pairwise_distance/pairwise_distance_numpy.py
+++ dpbench/benchmarks/pairwise_distance/pairwise_distance_numpy.py
@@ -4,7 +4,7 @@
 
 import numpy as np
 
+
 def pairwise_distance(X1, X2, D):
-    """Pairwise Numpy implementation using numpy linalg norm
-    """
+    """Pairwise Numpy implementation using numpy linalg norm"""
     np.copyto(D, np.linalg.norm(X1[:, None, :] - X2[None, :, :], axis=-1))

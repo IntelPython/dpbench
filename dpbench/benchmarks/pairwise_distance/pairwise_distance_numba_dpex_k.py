0d88deec2e5f30aaed6489da455e0e48bd643274
Wang, Mingjie1 <mingjie1.wang@intel.com>
2022-07-19 16:49:15 -0500

    Added pairwise_distance_numba_dpex_k.py. Added pairwise_distance_numba_dpex_n.py. Added pairwise_distance_numba_dpex_p.py.

---
+++ dpbench/benchmarks/pairwise_distance/pairwise_distance_numba_dpex_k.py
@@ -0,0 +1,24 @@
+# Copyright 2022 Intel Corp.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import numba_dpex
+import numpy as np
+
+
+@numba_dpex.kernel
+def pairwise_kernel(X1, X2, D):
+    i = numba_dpex.get_global_id(0)
+
+    N = X2.shape[0]
+    O = X1.shape[1]
+    for j in range(N):
+        d = 0.0
+        for k in range(O):
+            tmp = X1[i, k] - X2[j, k]
+            d += tmp * tmp
+        D[i, j] = np.sqrt(d)
+
+
+def pairwise_distance(X1, X2, D):
+    pairwise_kernel[X1.shape[0], numba_dpex.DEFAULT_LOCAL_SIZE](X1, X2, D)

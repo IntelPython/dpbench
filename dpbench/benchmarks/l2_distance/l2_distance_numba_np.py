45e3f50f3bb4d6f841bd70cf6eb078fa47935fb7
Adarsh Yoga <adarsh.yoga@intel.com>
2022-07-27 06:10:59 +0000

    pre-commit fixes

--- dpbench/benchmarks/l2_distance/l2_distance_numba_np.py
+++ dpbench/benchmarks/l2_distance/l2_distance_numba_np.py
@@ -3,9 +3,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import numpy as np
+
 import numba as nb
 
 nb.njit(parallel=True, fastmath=True)
+
+
 def l2_distance(a, b, d):
     sub = a - b
     sq = np.square(sub)

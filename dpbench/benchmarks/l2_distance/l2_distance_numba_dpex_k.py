6ee47d855f962cacb0d06b1e33b650073a859840
Wang, Mingjie1 <mingjie1.wang@intel.com>
2022-08-01 15:52:53 -0500

    Added l2_distance_numba_dpex_k.py, l2_distance_numba_dpex_n.py, l2_distance_numba_dpex_p.py

---
+++ dpbench/benchmarks/l2_distance/l2_distance_numba_dpex_k.py
@@ -0,0 +1,25 @@
+# Copyright 2022 Intel Corp.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import math
+import numba_dpex
+from numba_dpex import DEFAULT_LOCAL_SIZE, kernel
+
+atomic_add = numba_dpex.atomic.add
+
+@kernel(access_types={"read_only": ["a", "b"], "write_only": ["c"]})
+def l2_distance_kernel(a, b, c):
+    i = numba_dpex.get_global_id(0)
+    j = numba_dpex.get_global_id(1)
+    sub = a[i, j] - b[i, j]
+    sq = sub**2
+    atomic_add(c, 0, sq)
+
+
+
+def l2_distance(a, b, distance):
+    l2_distance_kernel[(a.shape[0], a.shape[1]), DEFAULT_LOCAL_SIZE](
+        a, b, distance
+    )
+    return math.sqrt(distance)
---
+++ dpbench/benchmarks/l2_distance/l2_distance_numba_dpex_p.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Intel Corp.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import numpy as np
+
+import numba as nb
+
+__njit = nb.njit(parallel=True, fastmath=True)
+
+
+@__njit
+def l2_distance_kernel(a, b):
+    sub = a - b
+    sq = np.square(sub)
+    sum = np.sum(sq)
+    d = np.sqrt(sum)
+    return d
+
+
+def l2_distance(a, b, _):
+    return l2_distance_kernel(a, b)

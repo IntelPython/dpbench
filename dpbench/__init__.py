5a31b562a52dde736147cb5b3cfd0ada1d6ecb1b
Diptorup Deb <diptorup.deb@intel.com>
2022-09-01 17:04:45 -0500

    Updates to runner.py
      - Rename the initialization modules to have a _initialize postfix.
      - Add a run_benchmark function

--- dpbench/__init__.py
+++ dpbench/__init__.py
@@ -5,11 +5,13 @@
 from .runner import (
     all_benchmarks_passed_validation,
     list_available_benchmarks,
+    run_benchmark,
     run_benchmarks,
 )
 
 __all__ = [
     "all_benchmarks_passed_validation",
+    "run_benchmark",
     "run_benchmarks",
     "list_available_benchmarks",
 ]

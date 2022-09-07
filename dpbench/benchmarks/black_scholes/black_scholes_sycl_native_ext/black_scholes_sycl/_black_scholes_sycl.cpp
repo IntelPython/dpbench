b9f6373bf61e5e726a29aa1ff4654b5c14e154c3
Wang, Mingjie1 <mingjie1.wang@intel.com>
2022-09-06 20:16:38 -0500

    Moved dpcpp kernel implementation into header file.

--- dpbench/benchmarks/black_scholes/black_scholes_sycl_native_ext/black_scholes_sycl/_black_scholes_sycl.cpp
+++ dpbench/benchmarks/black_scholes/black_scholes_sycl_native_ext/black_scholes_sycl/_black_scholes_sycl.cpp
@@ -13,75 +13,13 @@
 #include <stdlib.h>
 #include <type_traits>
 #include <vector>
+#include <iostream>
+#include "_black_scholes_kernel.hpp"
 
 using namespace sycl;
 namespace py = pybind11;
-
-#ifdef __DO_FLOAT__
-#define EXP(x) expf(x)
-#define LOG(x) logf(x)
-#define SQRT(x) sqrtf(x)
-#define ERF(x) erff(x)
-#define INVSQRT(x) 1.0f / sqrtf(x)
-
-#define QUARTER 0.25f
-#define HALF 0.5f
-#define TWO 2.0f
-#else
-#define EXP(x) sycl::exp(x)
-#define LOG(x) sycl::log(x)
-#define SQRT(x) sycl::sqrt(x)
-#define ERF(x) sycl::erf(x)
-#define INVSQRT(x) 1.0 / sycl::sqrt(x)
-
-#define QUARTER 0.25
-#define HALF 0.5
-#define TWO 2.0
-#endif
-
 namespace
 {
-
-template <typename FpTy>
-void black_scholes_impl(queue Queue,
-                        size_t nopt,
-                        const FpTy *price,
-                        const FpTy *strike,
-                        const FpTy *t,
-                        FpTy rate,
-                        FpTy volatility,
-                        FpTy *call,
-                        FpTy *put)
-{
-    // timer ON
-    auto e = Queue.submit([&](handler &h) {
-        h.parallel_for<class BlackScholesKernel>(
-            range<1>{nopt}, [=](id<1> myID) {
-                FpTy mr = -rate;
-                FpTy sig_sig_two = volatility * volatility * TWO;
-                int i = myID[0];
-                FpTy a, b, c, y, z, e;
-                FpTy d1, d2, w1, w2;
-
-                a = LOG(price[i] / strike[i]);
-                b = t[i] * mr;
-                z = t[i] * sig_sig_two;
-                c = QUARTER * z;
-                y = INVSQRT(z);
-                w1 = (a - b + c) * y;
-                w2 = (a - b - c) * y;
-                d1 = ERF(w1);
-                d2 = ERF(w2);
-                d1 = HALF + HALF * d1;
-                d2 = HALF + HALF * d2;
-                e = EXP(b);
-                call[i] = price[i] * d1 - strike[i] * e * d2;
-                put[i] = call[i] - price[i] + strike[i] * e;
-            });
-    });
-    e.wait();
-    // timer OFF
-}
 
 template <typename... Args> bool ensure_compatibility(const Args &...args)
 {
@@ -142,9 +80,9 @@ black_scholes_sync(size_t /**/,
     }
 
     black_scholes_impl(
-        Queue, nopt, (double *)price.get_data(), (double *)strike.get_data(),
-        (double *)t.get_data(), rate, volatility, (double *)call.get_data(),
-        (double *)put.get_data());
+        Queue, nopt, price.get_data<double>(), strike.get_data<double>(),
+        t.get_data<double>(), rate, volatility, call.get_data<double>(),
+        put.get_data<double>());
 }
 
 PYBIND11_MODULE(_black_scholes_sycl, m)

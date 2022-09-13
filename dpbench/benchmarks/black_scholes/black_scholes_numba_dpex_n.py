34239c7a1137f6644c8fdf8d4988652854721205
Diptorup Deb <diptorup.deb@intel.com>
2022-09-13 11:32:39 -0500

    Convert the blackscholes benchmark into a Python package.

--- dpbench/benchmarks/black_scholes/black_scholes_numba_dpex_n.py
+++ dpbench/benchmarks/black_scholes/black_scholes_numba_dpex_n.py
@@ -10,13 +10,13 @@ from numpy import exp, log, sqrt
 
 
 @nb.vectorize(nopython=True)
-def nberf(x):
+def _nberf(x):
     return erf(x)
 
 
 # blackscholes implemented using numpy function calls
 @nb.njit(parallel=True, fastmath=True)
-def black_scholes_kernel(price, strike, t, rate, vol, call, put):
+def _black_scholes(price, strike, t, rate, vol, call, put):
     mr = -rate
     sig_sig_two = vol * vol * 2
 
@@ -34,8 +34,8 @@ def black_scholes_kernel(price, strike, t, rate, vol, call, put):
     w1 = (a - b + c) * y
     w2 = (a - b - c) * y
 
-    d1 = 0.5 + 0.5 * nberf(w1)
-    d2 = 0.5 + 0.5 * nberf(w2)
+    d1 = 0.5 + 0.5 * _nberf(w1)
+    d2 = 0.5 + 0.5 * _nberf(w2)
 
     Se = exp(b) * S
 
@@ -45,4 +45,4 @@ def black_scholes_kernel(price, strike, t, rate, vol, call, put):
 
 
 def black_scholes(nopt, price, strike, t, rate, vol, call, put):
-    black_scholes_kernel(price, strike, t, rate, vol, call, put)
+    _black_scholes(price, strike, t, rate, vol, call, put)

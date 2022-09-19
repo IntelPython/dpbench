7164f37a43695d9b2a392f335245b164f8c24d18
Diptorup Deb <diptorup.deb@intel.com>
2022-09-19 11:01:56 -0500

    Fix dpcppFramework's validator function
        - DpcppFramework no longer needs to copy arrays back
          to numpy. The copy back is done in the teardown step
          of the execution.

--- dpbench/infrastructure/dpcpp_framework.py
+++ dpbench/infrastructure/dpcpp_framework.py
@@ -2,12 +2,10 @@
 #
 # SPDX-License-Identifier: Apache 2.0
 
-import os
-import pathlib
 import subprocess
-from typing import Any, Callable, Dict, Sequence, Tuple
+from typing import Callable
 
-from dpbench.infrastructure import Benchmark, Framework, utilities
+from dpbench.infrastructure import Framework
 
 
 class DpcppFramework(Framework):
@@ -54,57 +52,6 @@ class DpcppFramework(Framework):
         import dpctl.tensor as dpt
 
         return dpt.asnumpy
-
-    def validator(self) -> Callable:
-        """ """
-
-        def _validator(ref, test):
-            import dpctl.tensor as dpt
-
-            np_test = []
-            for t in test:
-                try:
-                    np_test.append(dpt.asnumpy(t))
-                except TypeError:
-                    print(
-                        "Failed to validate dpcpp results. Could not convert"
-                        + " dpcpp output to numpy ndarray"
-                    )
-                    return False
-            return utilities.validate(ref, np_test, framework=self.fname)
-
-        return _validator
-
-    def implementations(
-        self, bench: Benchmark
-    ) -> Sequence[Tuple[Callable, str]]:
-        """Returns the framework's implementations for a particular benchmark.
-        :param bench: A benchmark.
-        :returns: A list of the benchmark implementations.
-        """
-
-        module_pypath = "dpbench.benchmarks.{r}.{m}".format(
-            r=bench.info["relative_path"],
-            m=bench.info["module_name"] + "_sycl_native_ext",
-        ).replace(".", "/")
-
-        func_str = bench.info["func_name"] + "_sycl"
-        module_str = module_pypath.replace("/", ".")
-        ldict = dict()
-        try:
-            exec(
-                "from {m} import {f} as impl".format(m=module_str, f=func_str),
-                ldict,
-            )
-        except Exception as e:
-            print(
-                "Failed to load the {r} {f} implementation.".format(
-                    r=self.info["full_name"], f=func_str
-                )
-            )
-            raise e
-
-        return [(ldict["impl"], func_str)]
 
     def version(self) -> str:
         """Returns the framework version."""

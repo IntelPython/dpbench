IntelPython/dpbench

commit cd84df64ae22e4bbc0ff57036b2536e247320df6

Remove unused functions from numba_dpex_framework

dpbench/infrastructure/numba_dpex_framework.py
@@ -2,10 +2,9 @@
 #
 # SPDX-License-Identifier: Apache 2.0
 
-import pathlib
-from typing import Any, Callable, Dict, Sequence, Tuple
+from typing import Any, Dict
 
-from dpbench.infrastructure import Benchmark, Framework
+from dpbench.infrastructure import Framework
 
 _impl = {
     "kernel-mode": "k",
@@ -24,93 +23,9 @@ class NumbaDpexFramework(Framework):
 
         super().__init__(fname, fconfig_path)
 
-    def impl_files(self, bench: Benchmark) -> Sequence[Tuple[str, str]]:
-        """Returns the framework's implementation files for a particular
-        benchmark.
-        :param bench: A benchmark.
-        :returns: A list of the benchmark implementation files.
-        """
-
-        parent_folder = pathlib.Path(__file__).parent.absolute()
-        implementations = []
-        for impl_name, impl_postfix in _impl.items():
-            pymod_path = parent_folder.joinpath(
-                "..",
-                "..",
-                "dpbench",
-                "benchmarks",
-                bench.info["relative_path"],
-                bench.info["module_name"]
-                + "_"
-                + self.info["postfix"]
-                + "_"
-                + impl_postfix
-                + ".py",
-            )
-            implementations.append((pymod_path, impl_name))
-        return implementations
-
-    def implementations(
-        self, bench: Benchmark
-    ) -> Sequence[Tuple[Callable, str]]:
-        """Returns the framework's implementations for a particular benchmark.
-        :param bench: A benchmark.
-        :returns: A list of the benchmark implementations.
-        """
-
-        module_pypath = "dpbench.benchmarks.{r}.{m}".format(
-            r=bench.info["relative_path"].replace("/", "."),
-            m=bench.info["module_name"],
-        )
-        if "postfix" in self.info.keys():
-            postfix = self.info["postfix"]
-        else:
-            postfix = self.fname
-        module_str = "{m}_{p}".format(m=module_pypath, p=postfix)
-        func_str = bench.info["func_name"]
-
-        implementations = []
-        for impl_name, impl_postfix in _impl.items():
-            ldict = dict()
-            try:
-                exec(
-                    "from {m}_{p} import {f} as impl".format(
-                        m=module_str, p=impl_postfix, f=func_str
-                    ),
-                    ldict,
-                )
-                implementations.append((ldict["impl"], impl_name))
-            except ImportError:
-                continue
-            except Exception:
-                print(
-                    "Failed to load the {r} {f} implementation.".format(
-                        r=self.info["full_name"], f=impl_name
-                    )
-                )
-                continue
-
-        return implementations
-
     def imports(self) -> Dict[str, Any]:
         """Returns a dictionary any modules and methods needed for running
         a benchmark."""
         import dpctl
 
         return {"dpctl": dpctl}
-
-    def exec_str(self, bench: Benchmark, impl: Callable = None):
-        """Generates the execution-string that should be used to call
-        the benchmark implementation.
-        :param bench: A benchmark.
-        :param impl: A benchmark implementation.
-        """
-
-        dpctl_ctx_str = (
-            "with dpctl.device_context(dpctl.select_{d}_device()): ".format(
-                d=self.device
-            )
-        )
-        arg_str = self.arg_str(bench, impl)
-        main_exec_str = "__dpb_result = __dpb_impl({a})".format(a=arg_str)
-        return dpctl_ctx_str + main_exec_str

commit d3ef6bb0726a5e4ed6fe52b359e18e273f145025

Add an execute function in Framework class.
  -cd84df64ae introduced a bug in the framework class. We removed
   the exec_str function, but did not add a way for a Framework to
   execute an implementation. The result was, in the case of numba-dpex,
   all kernels were queued to the default queue no device_context
   was specified. The changes in the commit fixes the issue and
   adds a custom execute function with device_context inside
   the NumbaDpexFramework.

dpbench/infrastructure/benchmark.py
@@ -602,8 +602,7 @@ class Benchmark(object):
                         (
                             "Output did not match for {0}. "
                             + "Expected: {1} Actual: {2}"
-                        ).format(key, ref_out[key], frmwrk_out[key]),
-                        file=sys.stderr,
+                        ).format(key, ref_out[key], frmwrk_out[key])
                     )
             return valid
         except Exception:

dpbench/infrastructure/framework.py
@@ -6,7 +6,7 @@
 import json
 import pathlib
 import warnings
-from typing import Callable
+from typing import Callable, Dict
 
 import dpctl
 import numpy as np
@@ -98,8 +98,8 @@ class Framework(object):
             validator(ref_out, fw_out) -> bool
 
         where,
-            ref_out is a list of arrays with the reference results of a specific
-            benchmark
+            ref_out is a list of arrays with the reference results of a
+            specific benchmark
             fw_out is a list of arrays array with the results generated by the
             framework's implementation of the benchmark
         """
@@ -108,6 +108,14 @@ class Framework(object):
             return utilities.validate(ref, test, framework=self.fname)
 
         return _validator
+
+    def execute(self, impl_fn: Callable, input_args: Dict):
+        """A wrapper for a framework to customize how a benchmark
+        implementation should be executed.
+
+        :param impl: A benchmark implementation.
+        """
+        return impl_fn(**input_args)
 
     def __eq__(self, other):
         if not isinstance(other, type(self)):

dpbench/infrastructure/numba_dpex_framework.py
@@ -2,7 +2,9 @@
 #
 # SPDX-License-Identifier: Apache 2.0
 
-from typing import Any, Dict
+from typing import Any, Callable, Dict
+
+import dpctl
 
 from dpbench.infrastructure import Framework
 
@@ -29,3 +31,8 @@ class NumbaDpexFramework(Framework):
         import dpctl
 
         return {"dpctl": dpctl}
+
+    def execute(self, impl_fn: Callable, input_args: Dict):
+
+        with dpctl.device_context(self.sycl_device):
+            return impl_fn(**input_args)
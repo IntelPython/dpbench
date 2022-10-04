cf05ec672284dd14b4a81383fe1558babe01b2a4
Adarsh Yoga <adarsh.yoga@intel.com>
2022-10-04 20:35:43 +0000

    adding dpex prange implementation

---
+++ dpbench/benchmarks/knn/knn_numba_dpex_p.py
@@ -0,0 +1,30 @@
+# Copyright 2022 Intel Corp.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .knn_numba_npr import knn as knn_npr
+
+def knn(
+    x_train,
+    y_train,
+    x_test,
+    k,
+    classes_num,
+    test_size,
+    train_size,
+    predictions,
+    votes_to_classes,
+    data_dim
+):
+    knn_npr (
+        x_train,
+        y_train,
+        x_test,
+        k,
+        classes_num,
+        test_size,
+        train_size,
+        predictions,
+        votes_to_classes,
+        data_dim
+    )

1d580a60c6f05d2132ba56e4021c3d2d7cb69d4d
Adarsh Yoga <adarsh.yoga@intel.com>
2022-10-05 17:57:31 +0000

    pre-commit fixes

--- dpbench/benchmarks/black_scholes/black_scholes_numba_dpex_n.py
+++ dpbench/benchmarks/black_scholes/black_scholes_numba_dpex_n.py
@@ -4,5 +4,6 @@
 
 from .black_scholes_numba_np import black_scholes as bs_np
 
+
 def black_scholes(nopt, price, strike, t, rate, volatility, call, put):
     bs_np(nopt, price, strike, t, rate, volatility, call, put)
--- dpbench/benchmarks/black_scholes/black_scholes_numba_dpex_p.py
+++ dpbench/benchmarks/black_scholes/black_scholes_numba_dpex_p.py
@@ -4,5 +4,6 @@
 
 from .black_scholes_numba_npr import black_scholes as bs_npr
 
+
 def black_scholes(nopt, price, strike, t, rate, volatility, call, put):
     bs_npr(nopt, price, strike, t, rate, volatility, call, put)

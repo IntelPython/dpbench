c8039a05791ab10e3db10006355b58c2c3abe6ac
Adarsh Yoga <adarsh.yoga@intel.com>
2022-10-06 17:48:38 +0000

    adding pairwise distance numpy impls for numba_np and numba_dpex_n

--- dpbench/benchmarks/pairwise_distance/__init__.py
+++ dpbench/benchmarks/pairwise_distance/__init__.py
@@ -9,8 +9,14 @@ from .pairwise_distance_numba_dpex_k import (
 from .pairwise_distance_numba_dpex_p import (
     pairwise_distance as pairwise_distance_numba_dpex_p,
 )
+from .pairwise_distance_numba_dpex_n import (
+    pairwise_distance as pairwise_distance_numba_dpex_n,
+)
 from .pairwise_distance_numba_npr import (
     pairwise_distance as pairwise_distance_numba_npr,
+)
+from .pairwise_distance_numba_np import (
+    pairwise_distance as pairwise_distance_numba_np,
 )
 from .pairwise_distance_numpy import (
     pairwise_distance as pairwise_distance_numpy,
@@ -21,7 +27,31 @@ __all__ = [
     "initialize",
     "pairwise_distance_numba_dpex_k",
     "pairwise_distance_numba_dpex_p",
+    "pairwise_distance_numba_dpex_n",
     "pairwise_distance_numba_npr",
+    "pairwise_distance_numba_np",
     "pairwise_distance_numpy",
     "pairwise_distance_sycl",
 ]
+
+"""Pairwise distance computation of 2 n-dim arrays
+
+Input
+---------
+X1: double
+    first n-dim array
+X2: double
+    second n-dim array
+D : double
+    distance matrix
+
+Output
+-------
+d: array
+    pairwise distance
+
+Method
+------
+    D[i,j]+=sqrt((X1[i, k] - X2[j, k])^2)
+    here i,j are 0->number of points, k is 0->dims
+"""

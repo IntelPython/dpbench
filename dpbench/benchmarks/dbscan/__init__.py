c072b0e680d258c96c88db369c91153a7907189d
Wang, Mingjie1 <mingjie1.wang@intel.com>
2022-10-12 13:46:33 -0500

    Revised some tests' numpy implementation to python.
    1. renamed dbscan_numpy.py and knn_numpy.py to dbscan_python.py and knn_python.py.
    2. added python implementation of kmeans.

--- dpbench/benchmarks/dbscan/__init__.py
+++ dpbench/benchmarks/dbscan/__init__.py
@@ -7,7 +7,7 @@ from .dbscan_numba_dpex_k import dbscan as dbscan_numba_dpex_k
 from .dbscan_numba_dpex_p import dbscan as dbscan_numba_dpex_p
 from .dbscan_numba_n import dbscan as dbscan_numba_n
 from .dbscan_numba_npr import dbscan as dbscan_numba_npr
-from .dbscan_numpy import dbscan as dbscan_numpy
+from .dbscan_python import dbscan as dbscan_python
 
 __all__ = [
     "initialize",
@@ -15,5 +15,5 @@ __all__ = [
     "dbscan_numba_dpex_p",
     "dbscan_numba_n",
     "dbscan_numba_npr",
-    "dbscan_numpy",
+    "dbscan_python",
 ]

b7da854768842a57a878d6ecc764b2d4fcca1c06
Scherbakov, Denis <denis.scherbakov@intel.com>
2022-10-25 10:13:20 -0500

    port dpnp pairwise_distance impl from main_old

--- dpbench/benchmarks/pairwise_distance/pairwise_distance_dpnp.py
+++ dpbench/benchmarks/pairwise_distance/pairwise_distance_dpnp.py
@@ -5,5 +5,10 @@
 import dpnp
 
 def pairwise_distance(X1, X2, D):
-    """Pairwise Numpy implementation using numpy linalg norm"""
-    dpnp.copyto(D, dpnp.linalg.norm(X1[:, None, :] - X2[None, :, :], axis=-1))
+    x1 = dpnp.sum(dpnp.square(X1), axis=1)
+    x2 = dpnp.sum(dpnp.square(X2), axis=1)
+    D = -2 * dpnp.dot(X1, X2.T)
+    x3 = x1.reshape(x1.size, 1)
+    D += x3  # x1[:,None] Not supported by Numba
+    D += x2
+    D = dpnp.sqrt(D)

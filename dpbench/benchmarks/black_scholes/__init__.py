fae752c2a30b8bc8418018767e036bddaeeec74a
Ivan Butygin <ivan.butygin@gmail.com>
2022-12-07 16:58:04 +0100

    test

--- dpbench/benchmarks/black_scholes/__init__.py
+++ dpbench/benchmarks/black_scholes/__init__.py
@@ -18,7 +18,10 @@ all_benchmarks = [
     "numba_npr",
     "numpy",
     "python",
-    ("sycl", "black_scholes_sycl_native_ext"),
+    (
+        "sycl",
+        "black_scholes_sycl_native_ext.black_scholes_sycl._black_scholes_sycl",
+    ),
 ]
 
 __all__ = [

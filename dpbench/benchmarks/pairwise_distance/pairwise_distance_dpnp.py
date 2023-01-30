0513ebb1aacf0e323eaf6aa119f9ecaf8ced3d3f
Adarsh Yoga <adarsh.yoga@intel.com>
2023-01-30 23:06:40 +0000

    updating pairwise distance impl to be same as numba_dpex_n impl

--- dpbench/benchmarks/pairwise_distance/pairwise_distance_dpnp.py
+++ dpbench/benchmarks/pairwise_distance/pairwise_distance_dpnp.py
@@ -2,15 +2,15 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import dpnp
+import dpnp as np
 
 
 def pairwise_distance(X1, X2, D):
-    x1 = dpnp.sum(dpnp.square(X1), axis=1)
-    x2 = dpnp.sum(dpnp.square(X2), axis=1)
-    D = dpnp.dot(X1, X2.T)
+    x1 = np.sum(np.square(X1), axis=1)
+    x2 = np.sum(np.square(X2), axis=1)
+    np.dot(X1, X2.T, D)
     D *= -2
     x3 = x1.reshape(x1.size, 1)
-    D = dpnp.add(D, x3)
-    D = dpnp.add(D, x2)
-    D = dpnp.sqrt(D)
+    np.add(D, x3, D)
+    np.add(D, x2, D)
+    np.sqrt(D, D)

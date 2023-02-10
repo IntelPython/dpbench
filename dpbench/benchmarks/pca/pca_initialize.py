565abdd9176c96a5ca7e9cb24556feaecbec91ee
Adarsh Yoga <adarsh.yoga@intel.com>
2023-02-10 18:57:29 +0000

    adding pca implementations

---
+++ dpbench/benchmarks/pca/pca_initialize.py
@@ -0,0 +1,8 @@
+# Copyright 2022 Intel Corp.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+def initialize(npoints, dims):
+    from sklearn.datasets import make_regression
+    return make_regression(n_samples=npoints, n_features=dims)
---
+++ dpbench/benchmarks/pca/pca_numba_n.py
@@ -0,0 +1,33 @@
+# Copyright 2022 Intel Corp.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import numpy as np
+import numba as nb
+
+
+nb.njit(parallel=False, fastmath=True)
+def pca(data, dims_rescaled_data=2):
+    # mean center the data
+    data -= data.mean(axis=0)
+
+    # calculate the covariance matrix
+    v = np.cov(data, rowvar=False)
+
+    # calculate eigenvectors & eigenvalues of the covariance matrix
+    evalues, evectors = np.linalg.eigh(v)
+
+    # sort eigenvalues and eigenvectors in decreasing order
+    idx = np.argsort(evalues)[::-1]
+    evectors = evectors[:,idx]
+    evalues = evalues[idx]
+
+    # select the first n eigenvectors (n is desired dimension
+    # of rescaled data array, or dims_rescaled_data)
+    evectors = evectors[:, :dims_rescaled_data]
+
+    # carry out the transformation on the data using eigenvectors
+    tdata = np.dot(evectors.T, data.T).T
+
+    # return the transformed data, eigenvalues, and eigenvectors
+    return tdata, evalues, evectors

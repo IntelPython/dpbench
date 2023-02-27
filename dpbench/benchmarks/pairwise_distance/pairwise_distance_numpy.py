8b30927f1023f3e5f0444e2eb633c49b9d6f4222
Adarsh Yoga <adarsh.yoga@intel.com>
2023-02-27 22:59:13 +0000

    making pairwise distance consistent

--- dpbench/benchmarks/pairwise_distance/pairwise_distance_numpy.py
+++ dpbench/benchmarks/pairwise_distance/pairwise_distance_numpy.py
@@ -6,5 +6,11 @@ import numpy as np
 
 
 def pairwise_distance(X1, X2, D):
-    """Pairwise Numpy implementation using numpy linalg norm"""
-    np.copyto(D, np.linalg.norm(X1[:, None, :] - X2[None, :, :], axis=-1))
+    x1 = np.sum(np.square(X1), axis=1)
+    x2 = np.sum(np.square(X2), axis=1)
+    np.dot(X1, X2.T, D)
+    D *= -2
+    x3 = x1.reshape(x1.size, 1)
+    np.add(D, x3, D)
+    np.add(D, x2, D)
+    np.sqrt(D, D)

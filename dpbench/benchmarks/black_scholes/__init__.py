f73b23ff9a9ba9fad99915e4ea7631f3b9a3cf19
Adarsh Yoga <adarsh.yoga@intel.com>
2023-02-28 02:05:32 +0000

    updates to doc strings

--- dpbench/benchmarks/black_scholes/__init__.py
+++ dpbench/benchmarks/black_scholes/__init__.py
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: Apache 2.0
 """
-Documentation for black_scholes function
-
 The Black-Scholes program computes the price of a portfolio of
 options using partial differential equations.
 The entire computation performed by Black-Scholes is data-parallel

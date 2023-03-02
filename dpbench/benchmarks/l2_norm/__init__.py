9e3e843305944f277746bbef351672b4ac60a756
Adarsh Yoga <adarsh.yoga@intel.com>
2023-03-02 23:13:05 +0000

    adding l2 distance prage impls

--- dpbench/benchmarks/l2_norm/__init__.py
+++ dpbench/benchmarks/l2_norm/__init__.py
@@ -29,8 +29,10 @@ from .l2_norm_dpnp import l2_norm as l2_norm_dpnp
 from .l2_norm_initialize import initialize
 from .l2_norm_numba_dpex_k import l2_norm as l2_norm_numba_dpex_k
 from .l2_norm_numba_dpex_n import l2_norm as l2_norm_numba_dpex_n
+from .l2_norm_numba_dpex_p import l2_norm as l2_norm_numba_dpex_p
 from .l2_norm_numba_n import l2_norm as l2_norm_numba_n
 from .l2_norm_numba_np import l2_norm as l2_norm_numba_np
+from .l2_norm_numba_npr import l2_norm as l2_norm_numba_npr
 from .l2_norm_numpy import l2_norm as l2_norm_numpy
 from .l2_norm_sycl_native_ext import l2_norm_sycl
 
@@ -39,8 +41,10 @@ __all__ = [
     "l2_norm_dpnp",
     "l2_norm_numba_dpex_k",
     "l2_norm_numba_dpex_n",
+    "l2_norm_numba_dpex_p",
     "l2_norm_numba_n",
     "l2_norm_numba_np",
+    "l2_norm_numba_npr",
     "l2_norm_numpy",
     "l2_norm_sycl",
 ]

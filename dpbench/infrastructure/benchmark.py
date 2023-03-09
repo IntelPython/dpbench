6f4d33a6f00e35c95a9eeab87cfb4828799c1e14
Adarsh Yoga <adarsh.yoga@intel.com>
2023-03-09 01:07:16 +0000

    removing dpex kernel framework since it is the same as dpex framework

--- dpbench/infrastructure/benchmark.py
+++ dpbench/infrastructure/benchmark.py
@@ -26,7 +26,6 @@ from .dpnp_framework import DpnpFramework
 from .enums import ErrorCodes, ValidationStatusCodes
 from .framework import Framework
 from .numba_dpex_framework import NumbaDpexFramework
-from .numba_dpex_kernel_framework import NumbaDpexKernelFramework
 from .numba_framework import NumbaFramework
 
 
@@ -642,24 +641,13 @@ class Benchmark(object):
             elif "_python" in bimpl[0]:
                 impl_to_fw_map.update({bimpl[0]: Framework("python")})
             elif "_dpex" in bimpl[0]:
-                if "dpex_k" in bimpl[0]:
-                    try:
-                        fw = NumbaDpexKernelFramework("numba_dpex_kernel")
-                        impl_to_fw_map.update({bimpl[0]: fw})
-                    except Exception:
-                        logging.exception(
-                            "Framework could not be "
-                            + "created for numba_dpex kernel due to:"
-                        )
-                else:
-                    try:
-                        fw = NumbaDpexFramework("numba_dpex")
-                        impl_to_fw_map.update({bimpl[0]: fw})
-                    except Exception:
-                        logging.exception(
-                            "Framework could not be "
-                            + "created for numba_dpex due to:"
-                        )
+                try:
+                    fw = NumbaDpexFramework("numba_dpex")
+                    impl_to_fw_map.update({bimpl[0]: fw})
+                except Exception:
+                    logging.exception(
+                        "Framework could not be created for numba_dpex due to:"
+                    )
             elif "_sycl" in bimpl[0]:
                 try:
                     fw = DpcppFramework("dpcpp")

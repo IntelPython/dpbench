1be0c86b56df16af884129cace5d109e950e94b0
Adarsh Yoga <adarsh.yoga@intel.com>
2023-03-31 04:48:57 +0000

    restoring deleted impls

--- dpbench/benchmarks/kmeans/__init__.py
+++ dpbench/benchmarks/kmeans/__init__.py
@@ -1,21 +1,3 @@
 # Copyright 2022 Intel Corporation
 #
 # SPDX-License-Identifier: Apache 2.0
-
-from .kmeans_initialize import initialize
-from .kmeans_numba_dpex_k import kmeans as kmeans_numba_dpex_k
-from .kmeans_numba_dpex_p import kmeans as kmeans_numba_dpex_p
-from .kmeans_numba_n import kmeans as kmeans_numba_n
-from .kmeans_numba_npr import kmeans as kmeans_numba_npr
-from .kmeans_python import kmeans as kmeans_python
-from .kmeans_sycl_native_ext import kmeans_sycl
-
-__all__ = [
-    "initialize",
-    "kmeans_numba_dpex_k",
-    "kmeans_numba_dpex_p",
-    "kmeans_numba_n",
-    "kmeans_numba_npr",
-    "kmeans_python",
-    "kmeans_sycl",
-]

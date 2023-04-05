f27c43dbc3f422a81d1a6a7ed30a679ffa144ece
Adarsh Yoga <adarsh.yoga@intel.com>
2023-04-05 20:01:17 +0000

    Adding implementations of Rambo using numpy-like vector operations

---
+++ dpbench/benchmarks/rambo/rambo_python.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2022 - 2023 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import numpy as np
+
+
+def rambo(nevts, nout, C1, F1, Q1, output):
+    for i in range(nevts):
+        for j in range(nout):
+            C = 2.0 * C1[i, j] - 1.0
+            S = np.sqrt(1 - np.square(C))
+            F = 2.0 * np.pi * F1[i, j]
+            Q = -np.log(Q1[i, j])
+
+            output[i, j, 0] = Q
+            output[i, j, 1] = Q * S * np.sin(F)
+            output[i, j, 2] = Q * S * np.cos(F)
+            output[i, j, 3] = Q * C

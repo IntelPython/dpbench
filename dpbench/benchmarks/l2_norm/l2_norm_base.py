663b063c23d01154c870f3b551ac0207583262a0
Adarsh Yoga <adarsh.yoga@intel.com>
2023-04-06 05:17:02 +0000

    Re-work implementations to use new dpjit decorator

--- dpbench/benchmarks/l2_norm/l2_norm_base.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2023 Intel Corporation
-#
-# SPDX-License-Identifier: Apache-2.0
-
-import numpy as np
-
-
-def l2_norm(a, d):
-    d[:] = np.linalg.norm(a, axis=1)
--- dpbench/benchmarks/pca/pca_numba_dpex_n.py
+++ dpbench/benchmarks/pca/pca_numba_dpex_n.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import dpnp as np
+import numba as nb
 from numba_dpex import dpjit
 
 

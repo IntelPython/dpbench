627cab707f848b429e5b8e33ef3d13a402261ab1
Adarsh Yoga <adarsh.yoga@intel.com>
2023-05-01 18:36:01 +0000

    fixing complexity violations highlighted by flake8

--- dpbench/benchmarks/pairwise_distance/pairwise_distance_numba_npr.py
+++ dpbench/benchmarks/pairwise_distance/pairwise_distance_numba_npr.py
@@ -8,13 +8,13 @@ import numpy as np
 
 @nb.njit(parallel=True, fastmath=True)
 def pairwise_distance(X1, X2, D):
-    M = X1.shape[0]
-    N = X2.shape[0]
-    O = X1.shape[1]
-    for i in nb.prange(M):
-        for j in range(N):
+    X1_rows = X1.shape[0]
+    X2_rows = X2.shape[0]
+    X1_cols = X1.shape[1]
+    for i in nb.prange(X1_rows):
+        for j in range(X2_rows):
             d = 0.0
-            for k in range(O):
+            for k in range(X1_cols):
                 tmp = X1[i, k] - X2[j, k]
                 d += tmp * tmp
             D[i, j] = np.sqrt(d)

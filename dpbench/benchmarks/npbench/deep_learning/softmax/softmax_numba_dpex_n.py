49ee54acd8d82387d50d49950cc60679944ef287
Yevhenii Havrylko <yevhenii.havrylko@intel.com>
2023-05-01 18:07:14 -0400

    Add numba_dpex_n implementation for poly/np-bench

---
+++ dpbench/benchmarks/npbench/deep_learning/softmax/softmax_numba_dpex_n.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2021 ETH Zurich and the NPBench authors
+# SPDX-FileCopyrightText: 2022 - 2023 Intel Corporation
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import dpnp as np
+from numba_dpex import dpjit
+
+
+# Numerically-stable version of softmax
+@dpjit
+def softmax(x):
+    new_shape = (x.shape[0], x.shape[1], x.shape[2], 1)
+    # tmp_max = np.max(x, axis=-1, keepdims=True)
+    tmp_max = np.empty(new_shape, dtype=x.dtype)
+    for i in range(x.shape[3]):
+        tmp_max[:, :, :, 0] = np.max(x[:, :, :, i])
+    tmp_out = np.exp(x - tmp_max)
+    # tmp_sum = np.sum(tmp_out, axis=-1, keepdims=True)
+    tmp_sum = np.reshape(np.sum(tmp_out, axis=-1), new_shape)
+    return tmp_out / tmp_sum
---
+++ dpbench/benchmarks/polybench/linear-algebra/kernels/bicg/bicg_numba_dpex_n.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2010-2016 Ohio State University
+# SPDX-FileCopyrightText: 2021 ETH Zurich and the NPBench authors
+# SPDX-FileCopyrightText: 2022 - 2023 Intel Corporation
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import dpnp as np
+from numba_dpex import dpjit
+
+
+@dpjit
+def kernel(A, p, r):
+    return r @ A, A @ p

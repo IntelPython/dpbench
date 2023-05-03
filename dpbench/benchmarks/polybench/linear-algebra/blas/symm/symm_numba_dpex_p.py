5f39673853793d949fa8a4c891fe9f5846ed4c88
Yevhenii Havrylko <yevhenii.havrylko@intel.com>
2023-05-03 11:03:38 -0400

    Add numba_dpex_p implementation for poly/np-bench

---
+++ dpbench/benchmarks/polybench/linear-algebra/blas/symm/symm_numba_dpex_p.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2010-2016 Ohio State University
+# SPDX-FileCopyrightText: 2021 ETH Zurich and the NPBench authors
+# SPDX-FileCopyrightText: 2022 - 2023 Intel Corporation
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import dpnp as np
+import numba as nb
+from numba_dpex import dpjit
+
+
+@dpjit
+def kernel(alpha, beta, C, A, B):
+    temp2 = np.empty((C.shape[1],), dtype=C.dtype)
+    C *= beta
+    for i in range(C.shape[0]):
+        for j in nb.prange(C.shape[1]):
+            C[:i, j] += alpha * B[i, j] * A[i, :i]
+            temp2[j] = B[:i, j] @ A[i, :i]
+        C[i, :] += alpha * B[i, :] * A[i, i] + alpha * temp2

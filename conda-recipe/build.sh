26f0ceb04784232148cb547308129a4cb0003f77
Yevhenii Havrylko <yevhenii.havrylko@intel.com>
2024-04-30 18:01:21 -0400

    Migrate package build to python-build

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -16,18 +16,30 @@ export CMAKE_GENERATOR="Ninja"
 export CC=icx
 export CXX=icpx
 
-if [ -e "_skbuild" ]; then
-    ${PYTHON} setup.py clean --all
-fi
-
 # TODO: switch to pip build. Currently results in broken binary on Windows
 # $PYTHON -m pip install --no-index --no-deps --no-build-isolation . -v
 
-# Build wheel package
-if [ -n "${WHEELS_OUTPUT_FOLDER}" ]; then
-    $PYTHON setup.py install --single-version-externally-managed --record=record.txt bdist_wheel -p manylinux2014_x86_64 --build-number $GIT_DESCRIBE_NUMBER
-    mkdir -p ${WHEELS_OUTPUT_FOLDER}
-    cp dist/dpbench*.whl ${WHEELS_OUTPUT_FOLDER}
-else
-    $PYTHON setup.py install --single-version-externally-managed --record=record.txt
+# -wnx flags mean: --wheel --no-isolation --skip-dependency-check
+${PYTHON} -m build -w -n -x
+${PYTHON} -m wheel tags --remove --build "$GIT_DESCRIBE_NUMBER" \
+    --platform-tag manylinux2014_x86_64 dist/dpbench*.whl
+${PYTHON} -m pip install dist/dpbench*.whl \
+    --no-build-isolation \
+    --no-deps \
+    --only-binary :all: \
+    --no-index \
+    --prefix ${PREFIX} \
+    -vv
+
+# Must be consistent with pyproject.toml project.scritps. Currently pip does
+# not allow to ignore scripts installation, so we have to remove them manually.
+# https://github.com/pypa/pip/issues/3980
+# We have to let conda-build manage it for use in order to set proper python
+# path.
+# https://docs.conda.io/projects/conda-build/en/stable/resources/define-metadata.html#python-entry-points
+rm ${PREFIX}/bin/dpbench
+
+# Copy wheel package
+if [[ -v WHEELS_OUTPUT_FOLDER ]]; then
+    cp dist/dpbench*.whl "${WHEELS_OUTPUT_FOLDER[@]}"
 fi
